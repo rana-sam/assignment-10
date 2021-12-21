f5be8fc69048fb255155025376bce82770381eb2
Rana Sam <ranausama2082001@gmail.com>
2021-12-21 13:22:41 +0500

    program 7
    a js program to toggle case of each character of a string.

--- script.js
+++ script.js
@@ -2,86 +2,94 @@
 // Write a js program to find length of a string.
 
 function lengthOfString() {
-
-    var string='my name is usama'
-    var lengthString=string.length
-    console.log("Length Of String Is ",lengthString);
-    
+  var string = "my name is usama";
+  var lengthString = string.length;
+  console.log("Length Of String Is ", lengthString);
 }
-lengthOfString()
-
+lengthOfString();
 
 // program 2
 // Write a js program to copy one string to another string.
 function copyString() {
-    var String='Muhammad Usama'
-    var copy=''
-    var lengthof=String.length
-    lengthof=lengthof/2
-    var first=String.slice(0,lengthof)
-    var second=String.slice(lengthof+1,String.length)
-    copy=first+second
-    console.log("Copy Of String Is:",copy);
+  var String = "Muhammad Usama";
+  var copy = "";
+  var lengthof = String.length;
+  lengthof = lengthof / 2;
+  var first = String.slice(0, lengthof);
+  var second = String.slice(lengthof + 1, String.length);
+  copy = first + second;
+  console.log("Copy Of String Is:", copy);
 }
-copyString()
-
+copyString();
 
 // program 3
 // Write a js program to concatenate two strings.
 function concate() {
-    var string1='Muhammad'
-    var string2=' Usama Azam'
-    var concate= string1.concat(string2)
-    console.log("After concating:",concate);
-    
+  var string1 = "Muhammad";
+  var string2 = " Usama Azam";
+  var concate = string1.concat(string2);
+  console.log("After concating:", concate);
 }
-concate()
-
+concate();
 
 // program 4
 // Write a js program to compare two strings.
 function compare() {
-    var string1='usama azam'
-    var string2='usama azam'
-    var length1=string1.length
-    var length2=string2.length
-    var check=0
-    if (length1===length2) {
-        for(let x=0;x<length1;x++){
-            if (string1.charAt(x)==string2.charAt(x)) {
-                check++;
-            }
-        }
-        
+  var string1 = "usama azam";
+  var string2 = "usama azam";
+  var length1 = string1.length;
+  var length2 = string2.length;
+  var check = 0;
+  if (length1 === length2) {
+    for (let x = 0; x < length1; x++) {
+      if (string1.charAt(x) == string2.charAt(x)) {
+        check++;
+      }
     }
-    if (check==length1) {
-        console.log("Strings Are Equal");
-    }
-    else{
-        console.log("Strings Are Not Equal");
-    }
-    
+  }
+  if (check == length1) {
+    console.log("Strings Are Equal");
+  } else {
+    console.log("Strings Are Not Equal");
+  }
 }
-compare()
-
+compare();
 
 // program 5
 // Write a js program to convert lowercase string to uppercase.
 function uppercase() {
-    var string1='usamA azam'
-    var upper=string1.toUpperCase()
-    console.log("After Conversion Into Uppercase: ",upper);
-    
+  var string1 = "usamA azam";
+  var upper = string1.toUpperCase();
+  console.log("After Conversion Into Uppercase: ", upper);
 }
-uppercase()
-
+uppercase();
 
 // program 6
 // Write a js program to convert uppercase string to lowercase.
 function lowercase() {
-    var string1='Muhammad UsaMA'
-    var lower=string1.toLowerCase()
-    console.log("After Conversion Into Uppercase: ",lower);
-    
+  var string1 = "Muhammad UsaMA";
+  var lower = string1.toLowerCase();
+  console.log("After Conversion Into Uppercase: ", lower);
 }
-lowercase()
+lowercase();
+
+// program 7
+// Write a js program to toggle case of each character of a string.
+function togalcas() {
+  var string1 = "Muhammad UsaMA";
+  var alpha = "";
+  var z = "";
+  var togal;
+  for (let x = 0; x < string1.length; x++) {
+    alpha = string1.charAt(x);
+    if (alpha >= "a") {
+      var y = alpha.toUpperCase();
+      z = z.concat(y);
+    } else {
+      var y = alpha.toLowerCase();
+      z = z.concat(y);
+    }
+  }
+  console.log("After Togal: ", z);
+}
+togalcas();
